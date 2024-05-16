ef6f734f7f8b82c061b1b4027696c8593eecba73
Vladimir Gordeev <WorkStationGuard@gmail.com>
2024-05-16 21:37:50 +0300

    Работа с redis

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Cache::rememberForever('posts:all', function (){
+            return Post::all();
+        });
+
+        dd($posts->pluck('title')->toArray());
+    }
+
+    public function show($id)
+    {
+        $posts = Cache::get('posts:' . $id);
+        dd($posts->title);
+    }
+
+    public function store()
+    {
+//        $post =
+    }
+
+}
